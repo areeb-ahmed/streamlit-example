abc130644e3bd4fdf2cff95ebcccd7d4b6d6bd58
Areeb Ahmed <areeeb1@gmail.com>
2021-12-12 02:51:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,111 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+class menu:
+    def __init__(self, choice):
+        self.choice = choice
+        if choice == "A":
+            count = 0
+            file = open("reservation.txt")
+            lines = file.readlines()[1:]
+            file.close()
+            for line in lines:
+                count += 1
 
-"""
-# Welcome to Streamlit!
+            if count == 0:
+                print("There are no reservations!!")
+                print()
+            else:
+                file = open("reservation.txt", "r")
+                print(file.read())
+                file.close()
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+        elif choice == "B":
+            with open("reservation.txt", "r") as file:
+                for last_line in file:
+                    pass
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+            if last_line[0] == "#":
+                num = 1
+            else:
+                num = int(last_line[0]) + 1
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+            name = input("Enter Name: ")
+            date = input("Enter Date: ")
+            time = input("Enter Time: ")
+            adults = int(input("No. of Adults: "))
+            children = int(input("No. of Children: "))
+            file = open("reservation.txt", "a")
+            file.write(f"{num}\t\t\t{name}\t\t\t{date}\t\t\t{time}\t\t\t{adults}\t\t\t{children}\n")
+            file.close()
+            print()
+
+        elif choice == "C":
+            resnum = input("Enter Reservation number: ")
+            file1 = open("reservation.txt", "r")
+            lines = file1.readlines()
+            file1.close()
+            file2 = open("reservation.txt", "w")
+
+            for line in lines:
+                if not line.startswith(resnum):
+                    file2.write(line)
+            file2.close()
+
+        elif choice == "D":
+            adults, children, total_adults, total_children, total = 0, 0, 0, 0, 0
+            file = open("reservation.txt", "r")
+            list_of_lists=[]
+            report = ""
+            i = 0
+
+            for line in file:
+                i += 1
+                if i > 1:
+
+                    stripped_line = line.strip()
+                    line_list = stripped_line.split("\t\t\t")
+                    adults += int(line_list[4])
+                    children += int(line_list[5])
+                    subtotal = (int(line_list[4]) * 500) + (int(line_list[5]) * 300)
+                    total += subtotal
+                    line_list.append(str(subtotal))
+                    report += f"{line_list[0]}\t\t\t{line_list[2]}\t\t\t{line_list[3]}\t\t\t" \
+                              f"{line_list[1]}\t\t\t{line_list[4]}\t\t\t{line_list[5]}\t\t\t{line_list[6]}\n"
+            file.close()
+
+            print()
+            print()
+            print("                                                                       REPORT")
+            print()
+            print("#\t\t\tDate\t\t\t\tTime\t\t\t\tName\t\t\t\tAdults\t\t\tChildren\t\tSubtotal")
+            print(report)
+            print("Total Number of Adults: ", adults)
+            print("Total Number of Children: ", children)
+            print("Grand Total: PHP ", total)
+            print("---------------------------------------------------------------- nothing follows "
+                  "----------------------------------------------------------------")
+            print()
+            print()
+
+        elif choice == "E":
+            import sys
+            sys.exit("Thank you!")
+
+        else:
+            print("Invalid response. Please try again.")
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+while True:
+    try:
+        file = open("reservation.txt", "r")
+    except FileNotFoundError:
+        file = open("reservation.txt", "w+")
+        file.write("#\t\t\tName\t\t\t        Date\t\t\t        Time\t\t\t    Adults\t\t\tChildren\n")
+    file.close()
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    print("RESTAURANT RESERVATION SYSTEM")
+    print("System Menu:")
+    print("A. View all Reservations\tB. Make Reservation")
+    print("C. Delete Reservation\t\tD. Generate Report")
+    print("E. Exit\n")
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    selection_menu = input('Enter selection: ').upper()
+    menu(selection_menu)
